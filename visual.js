a0ed416976b5402e28e5d044b78c5592a0698033
flaribbit <1149761294@qq.com>
2019-09-14 19:56:13 +0800

    换用canvas

--- visual.js
+++ visual.js
@@ -1,11 +1,3 @@
-d3.select("svg").append('circle')
-    .attr('cx',30)
-    .attr('cy',40)
-    .attr('r',10)
-    .style('fill', '#000000')
-    .style('stroke', '#7F7F7F')
-    .style('stroke-width', 1)
-
 //读取文件
 document.getElementById('inputFile').onchange = function () {
     console.log('file loaded');
@@ -17,84 +9,108 @@ document.getElementById('inputFile').onchange = function () {
     fileReader.onload = function () {
         try {
             var data = d3.csvParse(this.result);
-            v=new visual(data);
-            for(var i=0;i<data.length;i++){
+            var v = new Visual(data);
+            for (var i = 0; i < 24; i++) {
                 v.addBubble(data[i]);
             }
-            v.draw();
+            v.Draw();
+            setInterval(() => {
+                v.Update();
+                v.Draw();
+            }, 100);
         } catch (error) {
             alert(error);
         }
     }
 }
 
-function visual(data) {
-    console.log(data);
-    this.bubbles = [];
-    this.border = {
-        top: 10,
-        bottom: -10,
-        left: -10,
-        right: 10
-    };
-    this.svg=document.getElementById('_svg');
-}
-
-visual.prototype.ramdom = function (lower, upper) {
-    var t = Math.random();
-    return (1 - t) * lower + t * upper;
-}
-
-visual.prototype.randomColor=function(){
-    var t=Math.floor(Math.random()*config.colors.length);
-    return config.colors[t];
-}
-
-//添加一个气泡，位置随机，颜色随机
-visual.prototype.addBubble = function (data) {
-    var bubble = {
-        id: data.name,
-        //x: this.ramdom(this.border.left, this.border.right),
-        //y: this.ramdom(this.border.bottom, this.border.top),
-        x: this.ramdom(0, 1280),
-        y: this.ramdom(0, 720),
-        r: data.value
-    };
-    this.bubbles.push(bubble);
-}
-
-//删除一个气泡
-visual.prototype.removeBubble = function () {
-
-}
-
-//更新气泡位置数据
-visual.prototype.update = function () {
-    
-}
-
-//绘制气泡
-visual.prototype.draw = function () {
-    for(var i=0;i<this.bubbles.length;i++){
-        var g;
-        if(g=document.getElementById(this.bubbles[i].id)){
-
-        }else{
-            g=document.createElementNS('http://www.w3.org/2000/svg','g');
-            g.setAttribute('transform','translate('+this.bubbles[i].x+','+this.bubbles[i].y+')');
-            var circle=document.createElementNS('http://www.w3.org/2000/svg','circle');
-            circle.setAttribute('cx',0);
-            circle.setAttribute('cy',0);
-            circle.setAttribute('r',this.bubbles[i].r);
-            circle.setAttribute('fill',this.randomColor());
-            var text=document.createElementNS('http://www.w3.org/2000/svg','text');
-            text.setAttribute('x',0);
-            text.setAttribute('y',0);
-            text.setAttribute('text-anchor','middle');
-            text.innerHTML=this.bubbles[i].id;
-            g.appendChild(circle);
-            g.appendChild(text);
-            this.svg.appendChild(g);
+class Visual {
+    constructor(data) {
+        console.log(data);
+        this.bubbles = [];
+        this.border = {
+            top: 0,
+            bottom: 720,
+            left: 0,
+            right: 1280,
+        };
+        this.ctx=document.getElementById("maincanvas").getContext("2d");
+        this.elasticity = 0.6;
+        this.scale= 1;
+    }
+    Random(lower, upper) {
+        var t = Math.random();
+        return (1 - t) * lower + t * upper;
+    }
+    RandomColor() {
+        var t = Math.floor(Math.random() * config.colors.length);
+        return config.colors[t];
+    }
+    //添加一个气泡，位置随机，颜色随机
+    addBubble(data) {
+        var bubble = {
+            id: data.name,
+            x: this.Random(this.border.left, this.border.right),
+            y: this.Random(this.border.bottom, this.border.top),
+            //x: this.Random(0, 320),
+            //y: this.Random(0, 240),
+            r: data.value,
+            c: this.RandomColor(),
+        };
+        this.bubbles.push(bubble);
+    }
+    //删除一个气泡
+    RemoveBubble() {
+    }
+    //更新气泡位置数据
+    Update() {
+        var distance, t, dx, dy, r;
+        this.scale*=1.08;
+        for (var i = 0; i < this.bubbles.length; i++) {
+            r=this.bubbles[i].r*this.scale;
+            //上边界
+            if ((t = this.bubbles[i].y - r - this.border.top) < 0) {
+                this.bubbles[i].y -= t * this.elasticity * 2;
+                this.scale*=0.99;
+            }
+            //下边界
+            if ((t = this.bubbles[i].y + r - this.border.bottom) > 0) {
+                this.bubbles[i].y -= t * this.elasticity * 2;
+                this.scale*=0.99;
+            }
+            //左边界
+            if ((t = this.bubbles[i].x - r - this.border.left) < 0) {
+                this.bubbles[i].x -= t * this.elasticity * 2;
+                this.scale*=0.99;
+            }
+            //右边界
+            if ((t = this.bubbles[i].x + r - this.border.right) > 0) {
+                this.bubbles[i].x -= t * this.elasticity * 2;
+                this.scale*=0.98;
+            }
+            //互相排斥
+            for (var j = i + 1; j < this.bubbles.length; j++) {
+                dx = this.bubbles[j].x - this.bubbles[i].x;
+                dy = this.bubbles[j].y - this.bubbles[i].y;
+                distance = Math.sqrt(dx * dx + dy * dy);
+                if ((t = distance - r - this.bubbles[j].r*this.scale) < 0) {
+                    t = t * this.elasticity / distance;
+                    this.bubbles[i].x += dx * t;
+                    this.bubbles[i].y += dy * t;
+                    this.bubbles[j].x -= dx * t;
+                    this.bubbles[j].y -= dy * t;
+                }
+            }
+        }
+    }
+    //绘制气泡
+    Draw() {
+        for(var i=0;i<this.bubbles.length;i++){
+            this.ctx.fillStyle=this.bubbles[i].c;
+            this.ctx.beginPath();
+            this.ctx.arc(this.bubbles[i].x,this.bubbles[i].y,this.bubbles[i].r,0,Math.PI*2);
+            this.cxt.closePath();
+            this.cxt.fill();
         }
     }
 }
